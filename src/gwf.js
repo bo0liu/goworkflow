dd6dfc02cb1d651ddadd3f44784843ad8863dfaf
bo0liu <boshi0liu@gmail.com>
2017-06-05 22:57:56 +0800

    Initial commit

---
+++ src/gwf.js
@@ -0,0 +1,22 @@
+var $ = go.GraphObject.make;
+var myDiagram =
+    $(go.Diagram, "myDiagramDiv",
+        {
+            initialContentAlignment: go.Spot.Center, // center Diagram contents
+            "undoManager.isEnabled": true // enable Ctrl-Z to undo and Ctrl-Y to redo
+        });
+
+var myModel = $(go.Model);
+// in the model data, each node is represented by a JavaScript object:
+myModel.nodeDataArray = [
+    { key: "Alpha" },
+    { key: "Beta" },
+    { key: "Gamma" }
+];
+myDiagram.model = myModel;
+myDiagram.nodeTemplate =
+    $(go.Node,
+        $(go.TextBlock,
+            // TextBlock.text is bound to Node.data.key
+            new go.Binding("text", "key"))
+    );
